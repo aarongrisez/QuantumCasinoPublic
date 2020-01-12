b56da27ea7ddef582623855e781fac505420d1b9
Aaron Grisez <grise100@mail.chapman.edu>
2020-01-12 14:52:39 -0800

    Adding docker-compose files for CI/testing

--- app/src/app.test.js
+++ app/src/app.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { useAuth0 } from './react-auth0-spa'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const user = {
+  email: 'test@bellga.me',
+  email_verified: true,
+  sub: 'google-oauth2|2147627834623744883746'
+}
+
+jest.mock('./react-auth0-spa');
+
+describe('App', () => {
+
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+    })
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
 });
