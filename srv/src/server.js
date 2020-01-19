62a1d75fc2bd4789a5717ebc7c855913af250d77
Aaron Grisez <grise100@mail.chapman.edu>
2020-01-19 15:19:34 -0800

    Updates to dockerfiles

--- srv/src/server.js
+++ srv/src/server.js
@@ -1,12 +1,24 @@
 const { Server, Mongo } = require('boardgame.io/server');
 const { TicTacToe } = require('./Games/TicTacToe')
 
-const server = Server({
-  games: [TicTacToe],
-  db: new Mongo({
-    url: 'mongodb://root:pass@mongo:27017',
-    dbname: 'games'
-  })
-});
+var successful_connect = false;
 
+while (!successful_connect) {
+  try {
+    const server = Server({
+      games: [TicTacToe],
+      db: new Mongo({
+        url: 'mongodb://root:pass@mongo:27017',
+        dbname: 'games'
+      })
+    });
+  }
+  catch(error) {
+    console.log(error);
+    console.log('Failed to connect to mongo, trying again in 5 seconds...');
+    setTimeout(5);
+  }
+}
+
+console.log('Server running, listening on 8000');
 server.run(8000);
